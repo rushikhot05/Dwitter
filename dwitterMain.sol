7be4865e4c8adf1cf767880d0a01ac0caa8357d8
Abbinaiya-Vijayaraghavan1 <56724139+Abbinaiya-Vijayaraghavan1@users.noreply.github.com>
2020-11-15 18:30:26 +0530

    Add files via upload

---
+++ dwitterMain.sol
@@ -0,0 +1,94 @@
+pragma solidity >=0.5.0;
+
+contract DwitterMain {
+   address owner;
+  
+   
+    struct Dweet {
+        uint id;
+        string content;
+        uint timestamp;
+        uint upvotes;
+        uint reports;
+        string hashtag;
+       
+    }
+   
+    struct User{
+        uint id;
+        address pkey;
+        string firstname;
+        string lastname;
+        string userName;
+        string bio;
+       // string photoUrl;  //profile picture
+    }
+   
+    uint public dweet_count=0;
+    uint public user_count=0;
+   
+    uint date = block.timestamp;
+   
+    Dweet[] public dweets;
+    User[] public users;
+   
+   
+    mapping(uint => address) public dweetToAuthor;  //maps dweet's id to author's address
+    mapping(address => uint) public dweetCountAuthor; //stores number of dweets by individual author
+    mapping(address => bool) accountCheck;  //only one account per public key
+    mapping(string => bool) userNameCheck;   //check if username already taken
+   // mapping(uint => string) DweetToHashtag;  
+   
+   event  NewUserAdd( string userName, address pkey);
+   event NewDweetAdd(string content, string hashtag, uint timestamp);
+   
+   
+   /*
+   
+     modifier onlyOwner(){
+         require(msg.sender == owner);
+         _;
+     }
+*/
+
+     modifier accountAlreadyExists(){
+        require(accountCheck[msg.sender] == false);
+        _;
+        //create alert in front end
+    }
+    
+    modifier userNameAlreadyExists(string memory _userName){
+        require(userNameCheck[_userName] == false);
+        _;
+        //create alert in front end
+        
+    }
+   
+    function addNewUser(string memory _firstName, string memory _lastName, string memory _userName, string memory _bio) public accountAlreadyExists userNameAlreadyExists(_userName){
+
+        users[user_count++]= User(user_count++, msg.sender, _firstName, _lastName, _userName, _bio);
+        accountCheck[msg.sender] = true;
+        userNameCheck[_userName] = true;
+        
+        // fire event new user added
+            emit NewUserAdd(_userName, msg.sender);
+       
+       
+    }
+   
+    function addNewDweet(string memory _content, string memory _hashtag) public{
+        uint dweetId = dweet_count++;
+        //string[] memory hashtagList = identifyHashtags(_content);  
+       
+        dweets[dweetId]= Dweet(dweetId, _content, block.timestamp, 0, 0, _hashtag);
+       
+        dweetToAuthor[dweetId] = msg.sender;
+        dweetCountAuthor[msg.sender]++;
+        
+        //fire event new dweet added
+        emit NewDweetAdd(_content, _hashtag, block.timestamp);
+       
+    }
+   
+   
+}
