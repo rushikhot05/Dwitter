a14cb7adb23ae4e79fd08e4520dea4515965b7c1
Abbinaiya-Vijayaraghavan1 <56724139+Abbinaiya-Vijayaraghavan1@users.noreply.github.com>
2020-11-30 23:22:20 +0530

    Update and rename dweet.sol to dwitterMain.sol

--- contracts/dwitterMain.sol
+++ contracts/dwitterMain.sol
@@ -27,13 +27,7 @@ contract DwitterMain {
         uint followers;
        // string photoUrl;  //profile picture
     }
-   
-  /* 
-   struct DweetsByAuthor{
-       mapping(address => uint) public AuthorToDweet;
-
-   } */
-   
+    
     uint public dweet_count=0;
     uint public user_count=0;
    
@@ -45,24 +39,19 @@ contract DwitterMain {
     mapping(uint => address) public dweetToAuthor;  //maps dweet's id to author's address
     mapping(address => uint) public dweetCountAuthor; //stores number of dweets by individual author
     mapping(address => bool) accountCheck;  //only one account per public key
-    mapping(string => bool) userNameCheck;   //check if username already taken
-   // mapping(uint => string) DweetToHashtag;  
+    mapping(string => bool) userNameCheck;   //check if username already taken  
    
    event  NewUserAdd( string userName, address pkey);
    event NewDweetAdd(string content, string hashtag, uint timestamp);
    
-
      modifier accountAlreadyExists(){
         require(accountCheck[msg.sender] == false);
         _;
-        //create alert in front end
     }
     
     modifier userNameAlreadyExists(string memory _userName){
         require(userNameCheck[_userName] == false);
         _;
-        //create alert in front end
-        
     }
     
     modifier userExists(){
@@ -72,39 +61,24 @@ contract DwitterMain {
     
     modifier userNameExists(string memory _userName){
         require(userNameCheck[_userName] == true);
-        _;
-        
+        _;   
     }
    
     function registerNewUser(string memory _firstName, string memory _lastName, string memory _userName, string memory _bio) public accountAlreadyExists userNameAlreadyExists(_userName){
-
         users.push(User(user_count, msg.sender, _firstName, _lastName, _userName, _bio,0,0));
         accountCheck[msg.sender] = true;
         userNameCheck[_userName] = true;
         userNameToId[_userName]= user_count;
-        addressToId[msg.sender]= user_count;
-        
+        addressToId[msg.sender]= user_count;   
         user_count++;
-        // fire event new user added
-            emit NewUserAdd(_userName, msg.sender);
-       
-       
+        emit NewUserAdd(_userName, msg.sender);
     }
    
     function addNewDweet(string memory _content, string memory _hashtag) userExists public{
-         
-        //string[] memory hashtagList = identifyHashtags(_content);  
-       
-        dweets.push(Dweet(dweet_count, _content, msg.sender, block.timestamp, 0, 0, _hashtag, false));
-       
+        dweets.push(Dweet(dweet_count, _content, msg.sender, block.timestamp, 0, 0, _hashtag, false)); 
         dweetToAuthor[dweet_count] = msg.sender;
         dweetCountAuthor[msg.sender]++;
-        
         dweet_count++;
-        //fire event new dweet added
         emit NewDweetAdd(_content, _hashtag, block.timestamp);
-       
     }
-   
-   
 }
