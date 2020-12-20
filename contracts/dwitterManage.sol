0eb6925af89913564e69fae578393400e356e067
Abbinaiya-Vijayaraghavan1 <56724139+Abbinaiya-Vijayaraghavan1@users.noreply.github.com>
2020-12-20 20:27:34 +0530

    Update dwitterManage.sol

--- contracts/dwitterManage.sol
+++ contracts/dwitterManage.sol
@@ -1,3 +1,4 @@
+
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0;
 import "./dwitterMain.sol";
@@ -7,24 +8,27 @@ contract DwitterManage is DwitterMain {
    //include display function
    
    event upvoted(uint id, address upvoter);
+    event downvoted(uint id, address downvoter);
    event reported(uint id, address reporter);
    event deleted(uint id, uint timestamp);
-   event followed(uint followers, uint following); 
+   event followed(uint followers, uint following);
    
    modifier onlyAuthor(uint id) {
        require(msg.sender == dweetToAuthor[id]);
        _;
    }
    
-   modifier dweetNotDeleted(uint id) {
-       require(dweets[id].deleted == false);
-       _;
-   }
-  
+
    function upvoteDweet(uint id) userExists public {
        dweets[id].upvotes++;
        emit upvoted(id, msg.sender);    
-   }   
+   }  
+
+   
+   function downvoteDweet(uint id) userExists public {
+       dweets[id].upvotes--;
+       emit downvoted(id, msg.sender);    
+   }  
    
     function reportDweet(uint id) userExists public {
        dweets[id].reports++;
@@ -32,7 +36,7 @@ contract DwitterManage is DwitterMain {
        if(dweets[id].reports > 10){
            dweets[id].deleted = true;
        }
-   }   
+   }  
    
    function deleteDweet(uint id) onlyAuthor(id) public {
        dweets[id].deleted = true;
@@ -47,33 +51,12 @@ contract DwitterManage is DwitterMain {
    
    function followUser(uint id) userExists  public {
        address toBeFollowed1= users[id].pkey;
-       require(msg.sender != toBeFollowed1); //a user cannot follow himself
+       require(msg.sender != toBeFollowed1); 
        users[id].followers++;
 
        uint id1 = addressToId[msg.sender];
        users[id1].following++;
        emit followed(users[id].followers, users[id1].following);
    }
-   
-  
-   function getDweetByUser(uint id, address _author) view public returns(string memory content, address author, uint timestamp,uint upvotes, uint reports, string memory hashtag)
-   {
-        Dweet memory dw = dweets[id];
-       if(_author == dw.author)
-         return (dw.content, dw.author, dw.timestamp, dw.upvotes, dw.reports, dw.hashtag);
-   } 
-   
-   function getDweetByHashtag(uint id, string memory _searchHashtag) dweetNotDeleted(id) view public returns(string memory content, address author, uint timestamp,uint upvotes, uint reports, string memory hashtag)
-   {
-       Dweet memory dw = dweets[id];
-       if(keccak256(abi.encodePacked(dw.hashtag)) == keccak256(abi.encodePacked(_searchHashtag)))
-       {
-           return (dw.content, dw.author, dw.timestamp, dw.upvotes, dw.reports, dw.hashtag);
-       }
-   }
-   
-   function getDweet(uint id) dweetNotDeleted(id) view public returns(string memory content, address author, uint timestamp,uint upvotes, uint reports, string memory hashtag) {
-       Dweet memory dw = dweets[id];
-       return (dw.content, dw.author, dw.timestamp, dw.upvotes, dw.reports, dw.hashtag);
-   }
 }
+
